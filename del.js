c3563ee2af4db491587edc76d57cd9b7e864acea
JeffreyBalaoro <balaorojeffrey218@gmail.com>
2022-10-20 10:35:18 +0800

    added delete.js

---
+++ del.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const del = function(id, oldNote){
+    const note = JSON.parse(oldNote)
+    const newNote = note.filter(function(n,idx){
+
+    })
+    fs.writeFileSync('./note.txt', JSON.stringfy(newNote));
+
+}
+module.exports = del
